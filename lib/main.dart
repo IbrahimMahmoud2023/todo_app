0b01a1d8d96559bfeeb1ac0d3e8de792e7d10414
IbrahimMahmoud2023 <embabyibrahim95@gmail.com>
2023-08-28 18:24:03 +0300

    create todo_app 1_splash view 2_home layout 3_task list screen UI 4_task_item_widget 5_task_list_view 6_add task 7_firebase added 8_complete firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:untitled/core/theme/app_theme.dart';
 import 'package:untitled/layout/home_layout.dart';
 import 'package:untitled/layout/moduls/splash/splash_view.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
